da1e175c59edaf73f3d98e332fa8af0aedc46c97
Max Max <m.rylkin@osome.com>
2023-08-13 15:49:11 +0300

    version

--- src/tracing.ts
+++ src/tracing.ts
@@ -4,9 +4,13 @@ import { SteroidExporter, getSteroidInstrumentation } from 'steroid-otel-utils';
 
 const projectRoot = process.cwd();
 
+const steroidApiUrl = process.env.NODE_END === 'development' ? 'http://127.0.0.1:3088' : undefined;
+
 const sdk = new opentelemetry.NodeSDK({
   autoDetectResources: true,
-  traceExporter: new SteroidExporter(),
+  traceExporter: new SteroidExporter({
+    apiUrl: steroidApiUrl,
+  }),
   instrumentations: [
     getSteroidInstrumentation({
       wrapCallSite: callSite => sourceMapSupport.wrapCallSite(callSite),
