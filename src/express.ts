434739f9b981801b3a0f40735a70a6dfa57e18f2
Max Max <m.rylkin@osome.com>
2023-08-14 16:11:04 +0300

    version

--- src/express.ts
+++ src/express.ts
@@ -9,24 +9,24 @@ import { getConnection } from "typeorm";
 
 const app = express();
 
-const sequelize = new Sequelize('sqlite::memory:', {
-    logging: false,
-} as Options);
+// const sequelize = new Sequelize('sqlite::memory:', {
+//     logging: false,
+// } as Options);
 
-const User = sequelize.define('User', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-}, { tableName: 'users', timestamps: false });
+// const User = sequelize.define('User', {
+//     id: {
+//         type: DataTypes.INTEGER,
+//         primaryKey: true,
+//         autoIncrement: true,
+//     },
+// }, { tableName: 'users', timestamps: false });
 
 app.get('/hello', async (req, res) => {
     const query = 'SELECT * FROM "users" WHERE "id" = :id';
 
-    await sequelize.query(query, { replacements: {id: 1} });
+    // await sequelize.query(query, { replacements: {id: 1} });
 
-    await User.findAll({ where: { id: 1 } });
+    // await User.findAll({ where: { id: 1 } });
 
     res.send('finish');
 });
@@ -35,7 +35,7 @@ app.get('/typeorm', exampleController);
 
 connectToDb()
     .then(() => Promise.all([
-        sequelize.query('CREATE TABLE "users" ("id" SERIAL PRIMARY KEY);'),
+        // sequelize.query('CREATE TABLE "users" ("id" SERIAL PRIMARY KEY);'),
         getConnection().query('CREATE TABLE "users" ("id" SERIAL PRIMARY KEY);'),
     ]))
     .then(() => app.listen(Number(process.env.PORT) || 3077))
